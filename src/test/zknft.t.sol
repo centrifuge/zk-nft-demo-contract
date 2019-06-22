d460e444d33aaee0f8b637831c0ecb49642787a9
Lucas Vogelsang <l@lucasvo.com>
2019-06-22 07:34:45 +0000

    Updated verifier

--- src/test/zknft.t.sol
+++ src/test/zknft.t.sol
@@ -50,44 +50,33 @@ contract ZKNFTTest is DSTest  {
         user2 = new User();
         anchors = new AnchorMock();
         nft = new ZKNFT("test", "TEST", address(anchors));
-        nft.file("ratings", bytes32(uint(0x58653f1572ef609a6576b89be3271f0f3e2d80669953c6f9cd2172a63bd5bac)));
     }
 
-    function getPoints() public returns (uint[8] memory) {
-        uint[8] memory points = [
-            0x17ff636d393ad540520e3babdedb27bd097b2fa2c33503a87f5f06e364da520e, 
-            0x26c43a5974be69877a806b64c8e8f7289b8e3226ed3fb0469d3d053a3f75b6e3,
-            0x0c77ac3e60830538356f581fa34c22b1a1eb2f8aa250e822eb7508b63371e0e7,
-            0x078a7eabdfc327beec7600720b03c222bdf136862866df7457bd70a994898e19,
-            0x02e856713cd5f4c1c912ee26369a97dca2fe4050e30051e4d4ebc7a60c3a6a0b,   
-            0x30518dbe4ad00edd19fc2a1e94fdea3d0c915f5d1353904b12b5e250631c04c3,
-            0x050b1558f013eb51e27211379b51c17394e9472805d9cdaf1678fca9ad969138,
-            0x06c3911a0bbdc014fe2bc7995015799f5a814b1fd951bc6a507f533b552aebca
-        ];
-        return points;
-    }
-
-    function testCheckAnchor() public logs_gas {
-        bytes32 sigs = 0x7619e5834eb2b4b13e4964435a32220518a72769897e8e313eb86e0ae69c81d9;
-        bytes32 data = 0x0053790d7ab6faebde5fb18ea1a7789c1728b4541e3f2662c29fad40a09d599a;
-        bytes32 root = 0x9e88392297bb8724039f7bf8f7be295a8f506e81d9038620107c7ab782a89ed4;
-        anchors.file(root, 0); 
-        require(nft.checkAnchor(1, data, sigs));
-    }
-    
     function testMint() public logs_gas {
-        //nft.file("ratings", 0x58653f1572ef609a6576b89be3271f0f3e2d80669953c6f9cd2172a63bd5bac);
-        bytes32 data_root = 0xc727cb3a59edcff5ab05ab8ab84fbf553c13d03ee99119aa4d1005cf4a4bcf6d;
-        
+        // Set the credit rating root on the NFT contract
+        nft.file("ratings", bytes32(uint(0x1e24c21a525a96ba9f00f4436167fad9bbb82722f9c22748ec84e462e6cbd7e3)));
+    
+        bytes32 sigs = 0x66e2b39e6df8e01df6af25c56bae2e80d9ec58617aad5f0bda7497bf85c8ea84; 
+        bytes32 root = 0x223dd9478e324771dde8320dcfe89a31e33a35d3dc1a00ca018bbc6d2606e4b0;
+        bytes32 data_root = 0xef8a19dbd60506d2ef7d9f48fc0452e3ccd5064393d91cd9639f4a57c4d5ba24;
+
         // Setting AnchorMock to return a given root
-        bytes32 sigs = 0xf29c33aee95eb75f20bb98e52ebc7497f28dc5e114c55bb8abefdbc839218378;
-        bytes32 root = 0x3599531e7357cd3c415736d9d6a854e143868dcdc16ca5663739b67569747515;
         anchors.file(root, 0); 
         
         uint rating = 0x0000000000000000000000000000000000000000000000000000000000000064;
         uint amount = 0x0000000000000000000000000000000000000000000000000000000000000140;
         
-        nft.mint(address(user1), 1, 1, data_root, sigs, amount, rating, getPoints());
+        uint[8] memory points =  [
+            0x0fdc3004e66336193e416adfdbf1386a3bb1897d6ffd81d2e0b388ab795f1463,
+            0x26e4ef80d5b7f22f4b728cc381b1233652251a47a750b142c34e9b91773443f4,
+            0x2c1ae2b2ba879614c9ee0a1b89697ae4bf4bd9187d292e4c3139ea31be70c00a,       
+            0x2458c01d1b0615bab2555516af46987e31e47e72edb49675e5b8ceaabdf0a7a0,
+            0x0cd751c8d760366dbb2abb09004c499f4531eea8160cecb9915e5edb92b0104a,
+            0x14b279c5aaa8b61e78f542bef7c89f07d70b12ac7bc3204ed4865f6d8bd6a912,
+            0x15b15fb2b3188f681730e91fb9ea194829f47430a4de559d6bc6e6648b121422,  
+            0x12aea6420fbe70b9d790a0b655080d9d05d0d9ca9681436971a5df449a4b352e];
+
+        nft.mint(address(user1), 1, 1, data_root, sigs, amount, rating, points);
         assertEq(address(user1), nft.ownerOf(1));
     }
 }
