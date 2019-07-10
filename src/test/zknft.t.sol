cfe4aee19df524d67d5707b935deaa4ec4648d68
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-07-10 16:47:37 +0200

    Fix test

--- src/test/zknft.t.sol
+++ src/test/zknft.t.sol
@@ -54,11 +54,11 @@ contract ZKNFTTest is DSTest  {
 
     function testMint() public logs_gas {
         // Set the credit rating root on the NFT contract
-        nft.file("ratings", bytes32(uint(0x1e24c21a525a96ba9f00f4436167fad9bbb82722f9c22748ec84e462e6cbd7e3)));
+        nft.file("ratings", bytes32(uint(0xaba3f2af3c5c97f5aa54b81af14eb156e1c66790472354804009125b56773228)));
     
-        bytes32 sigs = 0x66e2b39e6df8e01df6af25c56bae2e80d9ec58617aad5f0bda7497bf85c8ea84; 
-        bytes32 root = 0x223dd9478e324771dde8320dcfe89a31e33a35d3dc1a00ca018bbc6d2606e4b0;
-        bytes32 data_root = 0xef8a19dbd60506d2ef7d9f48fc0452e3ccd5064393d91cd9639f4a57c4d5ba24;
+        bytes32 sigs = 0x5d9215ea8ea2c12bcc724d9690de0801a1b9658014c29c2a26d3b89eaa65cd07;
+        bytes32 root = 0x86e2e64b15c97b7f8327165ff539097a745b7d44126f36978fc26cfb1c3e76fe;
+        bytes32 data_root = 0x7fdb7b2d4ddb3ca67c1a79725fc9b3e4e2b8d4c15bedc8cac1873fa58a75b837;
 
         // Setting AnchorMock to return a given root
         anchors.file(root, 0); 
@@ -67,14 +67,14 @@ contract ZKNFTTest is DSTest  {
         uint amount = 0x0000000000000000000000000000000000000000000000000000000000000140;
         
         uint[8] memory points =  [
-            0x0fdc3004e66336193e416adfdbf1386a3bb1897d6ffd81d2e0b388ab795f1463,
-            0x26e4ef80d5b7f22f4b728cc381b1233652251a47a750b142c34e9b91773443f4,
-            0x2c1ae2b2ba879614c9ee0a1b89697ae4bf4bd9187d292e4c3139ea31be70c00a,       
-            0x2458c01d1b0615bab2555516af46987e31e47e72edb49675e5b8ceaabdf0a7a0,
-            0x0cd751c8d760366dbb2abb09004c499f4531eea8160cecb9915e5edb92b0104a,
-            0x14b279c5aaa8b61e78f542bef7c89f07d70b12ac7bc3204ed4865f6d8bd6a912,
-            0x15b15fb2b3188f681730e91fb9ea194829f47430a4de559d6bc6e6648b121422,  
-            0x12aea6420fbe70b9d790a0b655080d9d05d0d9ca9681436971a5df449a4b352e];
+            0x245c5821e1ab7f55dcf88949ac6e0386767e21eec722896070cf9aa2623fc7ff,
+            0x1a5e30e3ad8b73c112f516eb9b2209ae9fb58bd0ad72c29dacf9c68ccd8b236a,
+            0x16f159ecf943c151c0e42e487ecac743d837af202ab69a1c459747a0fcd27f60,
+            0x12223aea498617271cf451e676881d499534b27dc03dbe654e41c28be9181c13,
+            0x019b966e82081a9a2bd4afc42cc95b56098d82cfc91b922c402dc66981caf477,
+            0x109f0795b87ecbe40774fd24a0a139ee4ca6ea458948da28c9f51aabb65be1bf,
+            0x28c7f141a9031264fef81dea045edb05925b6548792933f2c7fed53553099cd1,
+            0x28ca04fd476c030fe4e1045f19b75bc55626172adbc575e101c9a61142d8d0f5];
 
         nft.mint(address(user1), 1, 1, data_root, sigs, amount, rating, points);
         assertEq(address(user1), nft.ownerOf(1));
